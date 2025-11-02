ca6edd84b11f6dcfe2ae5fa2251c5a95411915eb
Hiroki Noda <kubo39@gmail.com>
2025-11-02 18:58:14 +0900

    window creation

--- source/app.d
+++ source/app.d
@@ -1,6 +1,24 @@
-import std.stdio;
+import bindbc.glfw;
+import bindbc.loader;
 
 void main()
 {
-    writeln("Hello, World!");
+    version(Windows)
+    {
+        const rc = loadGLFW("lib/glfw3.dll");
+        assert(rc == glfwSupport);
+    }
+    assert(glfwInit() != 0);
+    scope (exit) glfwTerminate();
+    assert(glfwVulkanSupported() != 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+    auto window = glfwCreateWindow(1280, 780, "", null, null);
+    assert(window !is null);
+    scope(exit) glfwDestroyWindow(window);
+    while (glfwWindowShouldClose(window) == GLFW_FALSE)
+    {
+        glfwPollEvents();
+    }
 }
